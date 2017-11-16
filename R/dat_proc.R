e5db1a6ca05b2a73d71672040d9e73a1d5a684e8
fawda123 <mbafs2012@gmail.com>
2017-11-16 15:17:44 -0800

    ASCI scores from Susie's raw data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -3,24 +3,59 @@ library(readr)
 # library(ASCI)
 devtools::load_all('../ASCI')
 
+
+######
+# pre-processing of Susie's raw data
+
 alg <- read.csv('data/algae.bug.data.092317.csv', stringsAsFactors = F)
 env <- read.csv('data/algae.site.data.092317.csv', stringsAsFactors = F)
 
-
-
 algprc <- getids(alg)
+envprc <- getids(env)
 
-rmv <- chkinp(algprc, env, getval = T)
+# check site matches
+chkinp(algprc, envprc)
+rmv <- chkinp(algprc, envprc, getval = T)
 algprc <- algprc %>%
   filter(!SampleID %in% rmv)
-rmv <- chkinp(algprc, env, getval = T)
+envprc <- envprc %>%
+  filter(!SampleID %in% rmv)
+
+# check taxonomy
+chkinp(algprc, envprc)
+rmv <- chkinp(algprc, envprc, getval = T)
 algprc <- algprc %>%
   filter(!FinalID %in% rmv)
-rmv <- chkinp(algprc, env, getval = T)
-algprc <- algprc %>% 
+
+# check if both sba, diatoms are present
+chkinp(algprc, envprc)
+rmv <- chkinp(algprc, envprc, getval = T)
+algprc <- algprc %>%
+  filter(!SampleID %in% rmv)
+envprc <- envprc %>%
   filter(!SampleID %in% rmv)
 
+# check diatom abundance data
+chkinp(algprc, envprc)
+rmv <- chkinp(algprc, envprc, getval = T)
+algprc <- algprc %>%
+  filter(!SampleID %in% rmv)
+envprc <- envprc %>%
+  filter(!SampleID %in% rmv)
 
+# check NA values in environmental predictors
+chkinp(algprc, envprc)
+rmv <- chkinp(algprc, envprc, getval = T) %>% 
+  .$SampleID %>% 
+  unique
+algprc <- algprc %>%
+  filter(!SampleID %in% rmv)
+envprc <- envprc %>%
+  filter(!SampleID %in% rmv)
 
+# final
+dat <- chkinp(algprc, envprc)
 
-ASCI(algprc, env)
+# get asci object and save
+results <- ASCI(dat$taxa, dat$site)
+save(results, file = 'data/results.RData', compress = 'xz')
